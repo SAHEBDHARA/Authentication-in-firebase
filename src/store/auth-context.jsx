03b6423aaeccb3ec4ba8f97e8b993120e17b877f
Saheb Dhara <sahebdhara961@gmail.com>
2023-10-07 10:16:07 +0530

    adding auto logout featured

--- src/store/auth-context.jsx
+++ src/store/auth-context.jsx
@@ -12,14 +12,48 @@ export const AuthContextProvider = (props) => {
   const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem('token', token);
-  };
+
+  
+
+  // When the user logs in
+const login = (token) => {
+  const loginTime = new Date().getTime();
+  localStorage.setItem('token', token);
+  localStorage.setItem('loginTime', loginTime);
+};
+
+const isSessionExpired = () => {
+  const loginTime = parseInt(localStorage.getItem('loginTime'));
+  const currentTime = new Date().getTime();
+  const fiveMinutesInMillis = 5 * 60 * 1000; // 5 minutes in milliseconds
+
+  return currentTime - loginTime > fiveMinutesInMillis;
+};
+const checkSession = () => {
+  if (isSessionExpired()) {
+    // Session has expired
+    alert('Your session has expired. Please log in again.');
+    logout();
+  }
+};
+
+// Run the session check every 30 seconds (you can adjust the interval)
+setInterval(checkSession, 30000); // 30 seconds in milliseconds
+
+
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token');
+    localStorage.removeItem('loginTime');
   };
+
+  window.addEventListener('beforeunload', (event) => {
+    if (isSessionExpired()) {
+      event.preventDefault();
+      event.returnValue = ''; // Show a confirmation dialog if needed
+    }
+  });
+  
 
   const contextValue = {
     token: token,
